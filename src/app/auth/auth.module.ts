2412cfc936d942019df99308568e05a5182fbe79
Yulia Lepner <ylepner@gmail.com>
2022-04-25 23:39:55 +0200

    feat: create login-page component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class AuthModule { }
